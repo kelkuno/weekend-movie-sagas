58d236302786a1653b18d67057aeb2f14412c354
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-08-26 16:28:10 -0500

    added back to list button on details component.

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,18 @@
-
+import {useHistory} from 'react-router-dom';
 function Details () {
 
+    const history = useHistory();
+
+    const handleBackBtn = () => {
+        console.log('back btn clicked');
+        history.push('/');
+
+    }//end of handleBackBtn
+
     return (
-        <h2>This is details. </h2>
+        <>
+        <button onClick={handleBackBtn}>Back to list!</button>
+        </>
     );
 
 }//end of Details component
