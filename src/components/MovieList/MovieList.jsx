bd2cbe59218c055b6491d1767a7e0a105fc769bb
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-08-26 16:32:43 -0500

    added client side routing functionality on movie image to movie detail

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 
 function MovieList() {
 
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+
+    const handleDetail = () => {
+        console.log('handle detail button');
+        history.push('/details');
+
+    }//end of handleDetail function
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
@@ -17,7 +25,7 @@ function MovieList() {
             <section className="movies">
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
+                        <div onClick={handleDetail} key={movie.id} >
                             <h3>{movie.title}</h3>
                             <img src={movie.poster} alt={movie.title}/>
                         </div>
